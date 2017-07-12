f2f914d3c579abf255434f4b2839bea2f25c1995
Mike Lee <git@iskitz.com>
2017-07-12 12:10:09 -0700

    ~/index.js: ++readability/

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
     , by: "mike.lee@iskitz"
     , at: "2017.07.12-07...2015"
 
-    , it: "Also demonstrates ionified literate programming."
+    , it: "Shows emoji faces that're all valid ions. It also demonstrates"
+        + " ionified literate programming in the way it's written."
+        
     , im: "Exploring ion API & runtime documentation syntax via"
         + " ~I: do documentation once; only 1st time."
         + " ~i: do documentation every time."
@@ -75,11 +77,11 @@
 , "choose a random face from the faces list":
     function chooseFace ()
       { ~ {I
-          :"Choose a face from the faces list by first generating a random"
+          :" choose a face from the faces list by first generating a random"
           +" number between 0 and the list's length. Next I use that number"
-          +" number to select the face at the matching position within the"
-          +" list. Lastly, I save the chosen face as a property on the faces"
-          +" list."
+          +" to select the face at the matching position within the list."
+          +" Lastly, I save the chosen face as a property on the faces list"
+          +" to enable easily & simply accessing it later actions."
           }
 
         var faces       = chooseFace.ion.faces
@@ -87,13 +89,13 @@
           ; choice      = Math.floor (choice)
           ; faces.next  = faces      [choice]
 
-        ~ {i:"Chose "+ faces.next +" using random number "+ choice}
+        ~ {i:"chose "+ faces.next +" using random number "+ choice}
       }
 
 , "show that face":
     function showFace ()
       { ~ {I
-          :"Show the chosen face by setting it as my web view's title and"
+          :" show the chosen face by setting it as my web view's title and"
           +" content."
           }
         document.title = document.body.innerHTML = showFace.ion.faces.next
@@ -102,7 +104,7 @@
 , "repeat 2014 times waiting <= 1.5 seconds each time":
     function repeat ()
       { ~ {I
-          :"Repeat this ion's do actions 2014 times waiting up to 1.5 seconds"
+          :" repeat this ion's do actions 2014 times waiting up to 1.5 seconds"
           +" each time. I remove the repeat action on the second repeat to"
           +" avoid doing all actions immediately *and* after a delay."
           }
