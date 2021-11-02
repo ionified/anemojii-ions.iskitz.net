7c2719c6c9bf23b9e63079615155eec8c81d16c1
Mike Lee <iskitz@users.noreply.github.com>
2021-11-02 00:47:01 -0700

    code & docs: index: more readable comma-first style

--- index.js
+++ index.js
@@ -5,30 +5,32 @@
     , is: "animated emoji ions"
     , by: 'mike.lee@iskitz'
     , on: -7.20151021
-    , to: -7.20200417
+    , to: -8.20210217
     , at: 15.5
     , it: "explores ionified literate programming while randomly displaying    "
         + "valid emoji face lions: literal ions: invoked object notations.     "
         + "Learn more about ions, ionify & ionified @ https://why.ionify.net/  "
-}
+    },
 
-, do:
+  do:
     [ "request emoji"
     ,   "await emoji"
     ,  "choose emoji"
     ,    "show emoji"
     , "wait .25 --> 1 second"
     , "repeat 20151021 times"
-    ]
+    ],
 
-, emoji
-:   null
+  emoji
+    : null
+    ,
 
-,"request emoji"
-:  {get:'emoji'}
+ "request emoji"
+    : {get:'emoji'}
+    ,
 
-,"await emoji"
-:   function awaitEmoji ()
+ "await emoji":
+    function awaitEmoji ()
       { ~
         { on:'anemojii.emoji@ions.iskitz.net'
             ,'anemojii.emoji@ions.iskitz.net'
@@ -42,38 +44,38 @@
           ; doing.length     = 2
 
         ~ {i:`set do to ${anemojii.do}`}
-      }
+      },
 
-, gotEmoji
-:   function gotEmoji ()
+  gotEmoji:
+    function gotEmoji ()
       { var anemojii       = gotEmoji.with
           ; anemojii.emoji = this
 
       ~ {i:`got emoji from: ${this.re.id} & will ${anemojii.do}`}
       ~ anemojii.do
-      }
+      },
 
-, "choose emoji"
-:   function chooseEmoji ()
+  "choose emoji":
+    function chooseEmoji ()
       { var emoji      = chooseEmoji.with.emoji
           ; emoji.next = emoji.new()
 
       ~ {i:`chose ${emoji.next}`}
-      }
+      },
 
-, "show emoji"
-:   function showEmoji ()
+  "show emoji":
+    function showEmoji ()
       { document.title = document.body.innerHTML = showEmoji.with.emoji.next
-      }
+      },
 
-, "wait .25 --> 1 second"
-:   function wait ()
+  "wait .25 --> 1 second":
+    function wait ()
       { wait.with.do.after = Math.random * 750 + 250 | 0
       ~ {i:`will wait ${wait.with.do.after} ms before showing the next emoji`}
-      }
+      },
 
-, "repeat 20151021 times"
-:   function repeat ()
+  "repeat 20151021 times":
+    function repeat ()
       { clearTimeout (repeat.last)
 
         var   todo   =  repeat.with.do
--- index.re.js
+++ index.re.js
@@ -4,25 +4,26 @@
     { id: 'anemojii.re@ions.iskitz.net'
     , by: 'mike.lee@iskitz'
     , on: -7.20151021
-    , to: -7.20190116
+    , to: -8.20210217
     , at: 15.0
     , is: "an exploration of animated emoji ions & ionified literate programming"
-    }
+    },
 
-, do:
+  do:
     [ "request emoji"
     , "await emoji"
     , "choose emoji"
     , "show emoji"
     , "wait .25 --> 1 second"
     , "repeat 20151021 times"
-    ]
+    ],
 
-, "request emoji"
-:   {I:" request anemoji's emoji ion."}
+  "request emoji":
+    {I:" request anemoji's emoji ion."}
+    ,
 
-, "await emoji"
-:   {I
+  "await emoji":
+    {I
     :" pause actions until emoji arrive by updating the do actions list to only"
     +" contain pending actions & by removing them from the active do actions"
     +" list before they're activated. Next, I add a sensor to observe when"
@@ -30,30 +31,30 @@
     +" simplify accessing them from anemojii's other actions. Finally, I resume"
     +" previously paused do actions by activating the updated do actions list of"
     +" pending actions."
-    }
+    },
 
-, "choose emoji"
-:   {I
+  "choose emoji":
+    {I
     :" choose an emoji by requesting it from anemoji's emoji ion. I then save the"
     +" chosen emoji as a property on the emoji list to simplify accessing it from"
     +" other actions."
-    }
+    },
 
-, "show emoji"
-:   {I:" show the chosen emoji as my web view's title & content."
-    }
+  "show emoji":
+    {I:" show the chosen emoji as my web view's title & content."
+    },
 
-, "wait .25 --> 1 second"
-:   {I
+  "wait .25 --> 1 second":
+    {I
     :" set the amount of time to wait after showing each emoji. I generate a"
     +" random length of time between 0 & 750 milliseconds then increase it by"
     +" 250 milliseconds to ensure waiting between a quarter to 1 second before"
     +" showing the next emoji. I keep this time as a property of anemojii's do"
     +" actions list to simplify accessing it from other actions."
-    }
+    },
 
-, "repeat 20151021 times"
-:   {I
+  "repeat 20151021 times":
+    {I
     :" repeat this ion's do actions list 20151021 times after waiting some"
     +" pre-calculated time each time. On the first repeat I remove the repeat"
     +" 20151021 times action from the list to avoid repeating all actions without"
